bb66852844cb7e6105a3a84266b7f5122e33e7c2
Nikhil  Chougule <nikhil.choughule@soroco.com>
2018-11-18 20:09:35 +0530

    mobile build done

---
+++ server/public/mobile/precache-manifest.283f2677c606341f4ecf075eefa18e7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/mobile/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "89315e768d2948b6181e",
+    "url": "/mobile/static/js/main.89315e76.chunk.js"
+  },
+  {
+    "revision": "5f43fda0dac721fe3f60",
+    "url": "/mobile/static/js/1.5f43fda0.chunk.js"
+  },
+  {
+    "revision": "89315e768d2948b6181e",
+    "url": "/mobile/static/css/main.3aa6c7eb.chunk.css"
+  },
+  {
+    "revision": "af6a6fde5047e01f58b998646841e2d4",
+    "url": "/mobile/index.html"
+  }
+];
