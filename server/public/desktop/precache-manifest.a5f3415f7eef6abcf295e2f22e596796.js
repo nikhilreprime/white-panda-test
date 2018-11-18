3fbf71593ad5ce27ee1ed304a3b86858e42bbae4
Nikhil  Chougule <nikhil.choughule@soroco.com>
2018-11-18 20:11:45 +0530

    desktop build done

---
+++ server/public/desktop/precache-manifest.a5f3415f7eef6abcf295e2f22e596796.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8a97a29a0540a48f7aff",
+    "url": "/static/js/main.8a97a29a.chunk.js"
+  },
+  {
+    "revision": "5f43fda0dac721fe3f60",
+    "url": "/static/js/1.5f43fda0.chunk.js"
+  },
+  {
+    "revision": "8a97a29a0540a48f7aff",
+    "url": "/static/css/main.9c539a85.chunk.css"
+  },
+  {
+    "revision": "23a0ffd367e6ee8eeb4acd302b35f3e2",
+    "url": "/index.html"
+  }
+];
